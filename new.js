2afcc7c737935babcbe30cbb0ccd216c940d161b
Gulay <gulaymovlamova@gmail.com>
2024-03-17 12:13:46 +0400

    feat: checked or not

--- new.js
+++ new.js
@@ -1,24 +1,25 @@
 const inputButton = document.querySelector('#input')
 const addButton = document.querySelector('#add')
+const taskContent = document.querySelector("#taskContent")
 
-
+let editIndex = ''
 const tasks = [
     { id: 1, name: 'Task 1', status: 'completed' }
 ]
 
 
 const createTask = () => {
-    const taskContent = document.querySelector("#taskContent")
+    
     let html = ''
-    for (let task of tasks) {
+    for (let index in tasks) {
         html += `<div class="flex justify-between items-center border-[#E1E1E1] border-[1px] h-[40px] px-[9px]">
         <div class="flex items-center">
-            <div><input onchange="changeStatus(${task}, event)" type="checkbox" ${task.status === 'completed' ? 'checked' : ''}></div>
-            <span id="${task.id}" class="font-700 text-[12px] leading-[16px] ml-[10px] ${task.status === 'completed' ? 'line-through' : ''}">${task.name}</span>
+            <div><input onchange="changeStatus(${index}, event)" type="checkbox" ${tasks[index].status === 'completed' ? 'checked' : ''}></div>
+            <span id="${tasks[index].id}" class="font-700 text-[12px] leading-[16px] ml-[10px] ${tasks[index].status === 'completed' ? 'line-through' : ''}">${tasks[index].name}</span>
         </div>
         <div class="flex items-center space-x-[8px]">
-            <div class="bg-[#0085FF] rounded-[5px] p-[5px]"><img class="size-[10px]" src="./images/pen.svg" alt=""></div>
-            <div class="bg-[#FF0000] rounded-[5px] p-[5px]"><img class="size-[10px]" src="./images/trash.svg" alt=""></div>
+            <button onclick="editElement(${index})" class="bg-[#0085FF] rounded-[5px] p-[5px]"><img class="size-[10px]" src="./images/pen.svg" alt=""></button>
+            <button onclick="deleteElement(${index})" class="bg-[#FF0000] rounded-[5px] p-[5px]"><img class="size-[10px]" src="./images/trash.svg" alt=""></button>
         </div>
     </div>`
     }
@@ -27,15 +28,36 @@ const createTask = () => {
 createTask()
 
 addButton.addEventListener('click', function (e) {
-    tasks.unshift({id: tasks.length + 1, name: inputButton.value, status: 'pending'})
+    if(editIndex.toString()){
+        tasks[editIndex].name = inputButton.value
+        editIndex = ''
+    }
+    else {
+        tasks.unshift({id: tasks.length + 1, name: inputButton.value, status: 'pending'})
+    }
+    
     createTask()
+    inputButton.value = ' ';
 })
 
 
 
-const changeStatus = (task ,event) => {
+const changeStatus = (index, event) => {
     let status = 'pending'
     if(event.target.checked) status = 'completed'
-    task.status = status
+    tasks[index].status = status
     createTask()
+
+}
+
+const deleteElement = (index) => {
+    const value = confirm('Do you want to delete this task?')
+    if(value === true) tasks.splice(index, 1)
+    
+    createTask()
+}
+
+const editElement = (index) => {
+    editIndex = index
+    inputButton.value = tasks[index].name
 }
